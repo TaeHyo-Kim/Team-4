1ed84b37d945e87cc20a80fbd2d287b5f4bbeb09
박정희 <psch2506@gmail.com>
2026-01-03 15:20:06 +0900

    데모 1.0 버전 구현

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-
+//
 // [1] 우리가 만든 기능들 import
 import 'features/auth/viewmodels.dart';
 import 'features/auth/views.dart';
